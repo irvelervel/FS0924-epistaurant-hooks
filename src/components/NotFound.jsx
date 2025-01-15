3ce07fea6c410783bad27ffa75f1d6f419c0e884
Stefano Casasola <ste.casasola@gmail.com>
2025-01-15 10:59:38 +0100

    Adds navigation and a couple of react-hooks

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,6 +1,9 @@
 import { Alert, Button, Col, Container, Row } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = function () {
+  const navigate = useNavigate()
+
   return (
     <Container>
       <Row className="justify-content-center my-3">
@@ -8,8 +11,17 @@ const NotFound = function () {
           <Alert variant="warning" className="text-center">
             <h3>404 - Not Found</h3>
             <p>
-              Ti sei perso? Clicca <Button variant="primary">QUI</Button> per
-              tornare in homepage!
+              Ti sei perso? Clicca
+              <Button
+                variant="primary"
+                onClick={() => {
+                  // window.location.assign('/') // <-- ricarica il browser :(
+                  navigate('/') // <-- modo corretto! senza ricaricare!
+                }}
+              >
+                QUI
+              </Button>
+              per tornare in homepage!
             </p>
           </Alert>
         </Col>
--- src/components/RestaurantNavbar.jsx
+++ src/components/RestaurantNavbar.jsx
@@ -1,20 +1,57 @@
 // creo un componente React per la navbar del mio ristorante
 import { Container, Navbar, Nav } from 'react-bootstrap'
+import { Link, useLocation } from 'react-router-dom'
+
+// REGOLE DEGLI HOOKS
+// 1) solo nei componenti a funzione
+// 2) prima del return, fuori da if/for/funzioni
 
 // i valori delle prop vanno tra doppi apici se sono stringhe, o tra { }
 // in ogni altro caso
 
 const RestaurantNavbar = function () {
+  const location = useLocation()
+  console.log('LOCATION OBJECT', location)
+
+  // location.pathname ci riporterà il PERCORSO CORRENTE
+  // posso utilizzarlo per capire se mi trovo in /, /prenota, /admin etc.
+  // sulla base di questa conoscenza, io posso illuminare il link corretto!
+
   return (
     <Navbar collapseOnSelect expand="md" bg="dark" data-bs-theme="dark">
       <Container fluid={true}>
-        <Navbar.Brand href="#home">Epistaurant</Navbar.Brand>
+        <Link to="/" className="navbar-brand">
+          Epistaurant
+        </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#features">Menu</Nav.Link>
-            <Nav.Link href="#pricing">Prenota</Nav.Link>
-            <Nav.Link href="#pricing">Admin</Nav.Link>
+            <Link
+              to="/menu"
+              className={
+                location.pathname === '/menu' ? 'nav-link active' : 'nav-link'
+              }
+            >
+              Menu
+            </Link>
+            <Link
+              to="/prenota"
+              className={
+                location.pathname === '/prenota'
+                  ? 'nav-link active'
+                  : 'nav-link'
+              }
+            >
+              Prenota
+            </Link>
+            <Link
+              to="/admin"
+              className={
+                location.pathname === '/admin' ? 'nav-link active' : 'nav-link'
+              }
+            >
+              Admin
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
